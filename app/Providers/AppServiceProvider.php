0580fc592acd2532a44a173f293a07b64152fb51
biggigt <d.kumarkhan@gmail.com>
2019-05-19 12:44:54 +0600

    Channels key shared publicly

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $channels = Channel::all();
+        View::share('channels', $channels);
     }
 }
