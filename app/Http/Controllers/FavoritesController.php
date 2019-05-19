f3f64d4053addd1e41512b6a32d5ab3035f83365
biggigt <d.kumarkhan@gmail.com>
2019-05-19 14:29:58 +0600

    Message voting feature implemented

---
+++ app/Http/Controllers/FavoritesController.php
@@ -0,0 +1,15 @@
+<?php
+
+class FavoritesController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(Reply $reply)
+    {
+        $reply->favorite();
+        return back();
+    }
+} 
--- app/Reply.php
+++ app/Reply.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    use Favoritable;
+
+    protected $guarded = [];
+    /* Активная загрузка (eager loading)  */
+    protected $with = ['owner', 'favorites'];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
